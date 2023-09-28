b78fd6e2d84ad86dfaf752ae075b5822761af44e
ranitan <ranitanagarajan@gmail.com>
2023-09-28 23:05:43 +0530

    task

--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Homeimg from '../Assets/Homeimage.jpg';
+import '../Styles/Home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div class='container-fluid'>
+      <div class='row'>
+        <div class='col-12'>
+          <img src={Homeimg} alt="Homeimage" class='img-fluid mt-5 ms-5 ps-5' width='90%'/>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
